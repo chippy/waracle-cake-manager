2c74f3f269f94433252a63bf0dbc3b825ef2fcb7
Tom Richardson <tom.chiken@gmail.com>
2022-04-20 21:59:27 +0100

    Set-up Routes

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,19 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import './App.css';
 
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import CakeList from './components/CakeList';
+import CakeForm from './components/CakeForm';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
       <MainContent>
-        <CakeList />
+        <Routes>
+          <Route path='/' element={<CakeList />} />
+          <Route path='/addcake' element={<CakeForm />} />
+        </Routes>
       </MainContent>
-    </>
+    </BrowserRouter>
   );
 }
 
---
+++ src/main/frontend/src/components/CakeForm.js
@@ -0,0 +1,5 @@
+const CakeForm = () => {
+  return <h1>Add a cake</h1>;
+};
+
+export default CakeForm;
