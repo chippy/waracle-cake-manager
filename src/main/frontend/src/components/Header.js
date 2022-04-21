34ed3cc41a3230296f84151dd5db485c02c5a3db
Tom Richardson <tom.chiken@gmail.com>
2022-04-21 14:47:59 +0100

    Add links to menu bar

--- src/main/frontend/src/components/Header.js
+++ src/main/frontend/src/components/Header.js
@@ -1,7 +1,13 @@
+import { NavLink } from "react-router-dom";
+
 const Header = () => {
   return (
-    <div className='ui fixed inverted top menu teal'>
-      <div className='ui container'></div>
+    <div className='ui fixed inverted orange top menu'>
+        <div class='ui header item'>
+          Waracle's Cakes
+        </div>
+        <NavLink to="/" className="item" activeClassName="item active">List Cakes</NavLink>
+        <NavLink to="/addcake" className="item center" activeClassName="item active">Add a Cake</NavLink>
     </div>
   );
 };
